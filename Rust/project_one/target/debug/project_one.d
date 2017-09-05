ae7f5268847d2207de077de6b660f884a995572c
Chad Baxter <cbaxter@mail.umw.edu>
2017-09-05 16:30:07 -0500

    added cargo init

---
+++ Rust/project_one/target/debug/project_one.d
@@ -0,0 +1,1 @@
+/home/cbax/Classes/CPSC240/Projects/ProjectOne/Rust/project_one/target/debug/project_one: /home/cbax/Classes/CPSC240/Projects/ProjectOne/Rust/project_one/src/main.rs
---
+++ Rust/project_one/target/rls/debug/libproject_one.d
@@ -0,0 +1,1 @@
+/home/cbax/Classes/CPSC240/Projects/ProjectOne/Rust/project_one/target/rls/debug/libproject_one.rmeta: /home/cbax/Classes/CPSC240/Projects/ProjectOne/Rust/project_one/src/main.rs
